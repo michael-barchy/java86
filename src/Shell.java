86f33480c7e46025eeb166d1b05987b19d5c9711
michael-barchy <michael.barchy@gmail.com>
2026-08-13 19:39:35 +0200

    shell v1

--- src/Shell.java
+++ src/Shell.java
@@ -2,18 +2,18 @@ public class Shell {
     public static void main(String[] args) {
         Native.print("Welcome !\r\n");
         String input = " ";
-        while (StringUtils.equals("exit", input)) {
+        while ("exit" != input) {
             Native.print("# ");
             input = Native.input();
             Native.print("\r\n");
-            if (StringUtils.equals("help", input)) {
+            if ("help" == input) {
                 Native.print("Command shell usage\r\n");
                 Native.print("-------------------\r\n");
                 Native.print("    help     Display this help message\r\n");
                 Native.print("    exit     Exit the command shell\r\n");
                 Native.print("\r\n");
             } else {
-                if (!StringUtils.equals("exit", input)) {
+                if ("exit" != input) {
                     Native.print("Unknow command ");
                     Native.print(input);
                     Native.print(". Type help to list available commands.\r\n");
